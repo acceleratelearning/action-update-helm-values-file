eba475e80428988fccd6bd2ba15c33be26ab696e
Rob Cannon <rcannon@acceleratelearning.com>
2022-07-21 13:20:21 -0400

    Skipping the user confirmation prompt to install a module

--- action.ps1
+++ action.ps1
@@ -8,7 +8,7 @@ param (
     [String] $GitHubAppKey = $env:INPUT_GITHUB_APP_KEY
 )
 
-Install-Module -Name powershell-yaml -Scope CurrentUser
+Install-Module -Name powershell-yaml -Scope CurrentUser -Confirm:$False 
 
 function Base64UrlEncodeBytes([Byte[]] $bytes) {
     [Convert]::ToBase64String($bytes) -replace '\+', '-' -replace '/', '_' -replace '='
